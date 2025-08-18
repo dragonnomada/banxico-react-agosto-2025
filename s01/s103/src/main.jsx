7af743acdd8ace7970ac3592b137b316fd5472c4
Alan Badillo Salas <dragonnomada123@gmail.com>
2025-08-18 10:34:40 -0600

    s103 - Versión estática

--- s01/s103/src/main.jsx
+++ s01/s103/src/main.jsx
@@ -1,9 +1,22 @@
 import { createRoot } from "react-dom/client"
 
+import "./styles.css"
+
 const root = createRoot(
     document.getElementById("root")
 )
 
+// Opción 1 - Componente Clase
+
+// Opción 2 - Componente Fucional
+
+let visita = Math.random().toString(10).slice(2) % 1_000
+
 root.render(
-    <h1>hola</h1>
+    <div>
+        <h1>Bienvenido</h1>
+        <img src="https://laeconomia.com.mx/wp-content/uploads/banco-de-mexico.gif.webp" />
+        <button>Acceder</button>
+        <span>Tu eres el visitante ({visita})</span>
+    </div>
 )

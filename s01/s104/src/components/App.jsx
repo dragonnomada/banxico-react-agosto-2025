022b9d70c3905b9ebd06fe4623ae9ded085f1d5c
Alan Badillo Salas <dragonnomada123@gmail.com>
2025-08-18 11:22:04 -0600

    s104 - Versión inicial (pinta home)

---
+++ s01/s104/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+import { Home } from "./Home"
+
+export class App extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return <Home />
+    }
+
+}
--- s01/s104/src/main.jsx
+++ s01/s104/src/main.jsx
@@ -1,5 +1,17 @@
 import { createRoot } from "react-dom/client"
 
+// Importación/Exportación por defecto:
+// App.jsx -> export default class/function App
+// Other.jsx -> import App from "../<path>/App"
+
+// Importación/Exportación parcial:
+// App.jsx -> export class/function App
+// Other.jsx -> import { App, ... } from "../<path>/App"
+
+import { App } from "./components/App"
+
 const root = createRoot(
     document.getElementById("root")
 )
+
+root.render(<App />)

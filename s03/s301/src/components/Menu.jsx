500a56181e35170d891f0a4a3ad1746436c9d4f0
Alan Badillo Salas <dragonnomada123@gmail.com>
2025-08-20 09:54:20 -0600

    s301 - Menú

--- s03/s301/src/components/Menu.jsx
+++ s03/s301/src/components/Menu.jsx
@@ -1,27 +1,28 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef } from "react"
 
-export function Menu({ open, onClose }) {
+// Responsabilidad:
+// Mostrar un panel genérico y notificar si se da click fuera de él
+export function Menu({ children, title, open, onClose }) {
 
+    const backdropRef = useRef()
     const menuRef = useRef()
 
-    const [count, setCount] = useState(0)
-
     useEffect(() => {
-        console.log("menú montado")
-
         // El controlador es el ancla (identificador)
         // que permite suscribir y desuscribir
         const onClick = event => {
-            console.log("click", event)
+            if (!open) return
+
+            if (!menuRef.current) return
+            if (!backdropRef.current) return
+
             const target = event.target
-            console.log(menuRef.current)
-            // if (menuRef.current.contains(target)) {
-            //     alert("Contenido")
-            // } else {
-            //     alert("Fuera")
-            // }
+
             if (!menuRef.current.contains(target)) {
-                if (onClose) onClose()
+                if (backdropRef.current.contains(target)) {
+                    console.log("Click fuera", open)
+                    if (onClose) onClose()
+                }
             }
         }
 
@@ -29,39 +30,41 @@ export function Menu({ open, onClose }) {
         window.addEventListener("click", onClick)
 
         return () => {
-            console.log("menú desmontado")
             window.removeEventListener("click", onClick)
         }
-    }, [])
-
-    useEffect(() => {
-        const id = setTimeout(() => {
-            setCount(count + 1)
-        }, 1_000)
-        return () => {
-            clearTimeout(id)
-        }
-    }, [count])
+    }, [open])
 
     if (!open) return null
 
     return (
         <div
-            id={Math.random().toString(10)}
-            ref={menuRef}
+            ref={backdropRef}
             style={{
                 position: "absolute",
                 left: "0px",
                 top: "0px",
                 height: "100vh",
-                width: "300px",
-                background: "whitesmoke",
-                borderRight: "1px solid gainsboro"
+                width: "100vw",
+                // background: "rgba(0, 0, 0, 0.5)",
+                background: "rgba(0, 0, 0, 0)",
             }}
         >
-            <p>
-                <span>Hola {count}</span>
-            </p>
+            <div
+                id={Math.random().toString(10)}
+                ref={menuRef}
+                style={{
+                    position: "absolute",
+                    left: "0px",
+                    top: "0px",
+                    height: "100vh",
+                    width: "300px",
+                    background: "whitesmoke",
+                    borderRight: "1px solid gainsboro"
+                }}
+            >
+                {title}
+                {children}
+            </div>
         </div>
     )
 
--- s03/s301/src/v1/App.jsx
+++ s03/s301/src/v1/App.jsx
@@ -1,11 +1,70 @@
-import { Menu } from "../components/Menu";
+import { useState } from "react"
+import { Menu } from "../components/Menu"
 
 export default function App() {
+
+    // Estado compartido
+    // Superior al menú y al botón (ambos tienen acceso)
+    const [openMenu, setOpenMenu] = useState(false)
 
     return (
         <div>
             <h1>Bienvenido</h1>
-            <Menu />
+            <Menu
+                open={openMenu}
+                // Cuando el menú notifica que debe cerrarse
+                // (detectó un click fuera del menú / dentro de su backdrop)
+                onClose={() => {
+                    // Siempre que se dé clic afuera
+                    // decidimos cerrar el menú
+                    setOpenMenu(false)
+                }}
+                // children={"Hola"} // Menos común
+                title={(
+                    <h1>
+                        <span style={{color: "tomato"}}>Banxico</span>
+                        <span>Menú Principal</span>
+                    </h1>
+                )}
+                // title="Hola mundo"
+            >
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: "0.5rem",
+                        padding: "1rem"
+                    }}
+                >
+                    <button>Pulsame 1</button>
+                    <button
+                        onClick={() => {
+                            setOpenMenu(false)
+                        }}
+                    >
+                        cerrar
+                    </button>
+                </div>
+            </Menu>
+            <button
+                onClick={() => {
+                    // Siempre que se dé click al botón
+                    // decidimos abrir el menú
+                    // (mantemos el estado como true)
+                    setOpenMenu(true)
+                }}
+            >
+                Abrir menú
+            </button>
+            <input
+                onKeyDown={event => {
+                    if (event.key === "Enter") {
+                        if (event.target.value === "abrir") {
+                            setOpenMenu(true)
+                        }
+                    }
+                }}
+            />
         </div>
     )
 

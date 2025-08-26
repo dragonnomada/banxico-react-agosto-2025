5c4f936ac37a22842ebf860893e11ddfcab2571c
Alan Badillo Salas <dragonnomada123@gmail.com>
2025-08-26 10:51:41 -0600

    s703 - v1 y v2 consolidadas (separación de los hooks)

--- s07/s703/src/v1/components/TableUsers.jsx
+++ s07/s703/src/v1/components/TableUsers.jsx
@@ -1,55 +1,5 @@
-import { useEffect } from "react"
 import { useState } from "react"
-
-export function useUsersQuery(options = { autoFetch: true }) {
-
-    const [users, setUsers] = useState([])
-    const [error, setError] = useState(null)
-    const [isLoading, setIsLoading] = useState(false)
-
-    const refetch = async (search = "") => {
-        setIsLoading(true)
-        setError(null)
-
-        try {
-            const response = await fetch("https://geocarta.org/api/curso/banxico/user/all", {
-                method: "post",
-                headers: {
-                    "content-type": "application/json"
-                },
-                body: JSON.stringify({
-                    search
-                })
-            })
-            if (response.ok) {
-                const { users, error } = await response.json()
-                if (error) throw error
-                else {
-                    setUsers(users)
-                }
-            } else throw `Error al consultar los usuarios (HTTP/${response.status})`
-        } catch (error) {
-            setError(`${error}`)
-        } finally {
-            setIsLoading(false)
-        }
-    }
-
-    useEffect(() => {
-        if (options?.autoFetch) {
-            refetch()
-        }
-    }, [])
-
-    return {
-        users,
-        error,
-        isError: !!error,
-        isLoading,
-        refetch,
-    }
-
-}
+import { useUsersQuery } from "../hooks/useUserQuery"
 
 export default function TableUsers() {
 
--- s07/s703/src/v2/components/TableUsers.jsx
+++ s07/s703/src/v2/components/TableUsers.jsx
@@ -1,40 +1,5 @@
-import { useEffect } from "react"
 import { useState } from "react"
-import { userAllApi } from "../../api/userAllApi"
-
-export function useUsersQuery(options = { autoFetch: true }) {
-
-    const [users, setUsers] = useState([])
-    const [error, setError] = useState(null)
-    const [isLoading, setIsLoading] = useState(false)
-
-    const refetch = async (search = "") => {
-        setIsLoading(true)
-        setError(null)
-
-        const [users, error] = await userAllApi(search)
-        
-        if (error) setError(error)
-        else setUsers(users)
-
-        setIsLoading(false)
-    }
-
-    useEffect(() => {
-        if (options?.autoFetch) {
-            refetch()
-        }
-    }, [])
-
-    return {
-        users,
-        error,
-        isError: !!error,
-        isLoading,
-        refetch,
-    }
-
-}
+import { useUsersQuery } from "../hooks/useUserQuery"
 
 export default function TableUsers() {
 
